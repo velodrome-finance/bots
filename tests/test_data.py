424034d710a05cd22bf133e43a29527acca4e144
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-10-30 16:42:46 +0000

    fix linter

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv(".env.example")
 
-from bots.settings import TOKEN_ADDRESS
-from bots.data import Token, Price
+from bots.settings import TOKEN_ADDRESS # noqa
+from bots.data import Token, Price # noqa
 
 
 @pytest.mark.asyncio
 async def test_get_by_token_address():
     t = await Token.get_by_token_address(TOKEN_ADDRESS)
     assert t.token_address == TOKEN_ADDRESS
-    assert t.listed == True
+    assert t.listed is True
 
 
 @pytest.mark.asyncio
