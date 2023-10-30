121a5cfa61cf7e39fd92c403f5c0d4e16953d0cc
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-10-30 16:20:21 +0000

    lint

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,16 +1,19 @@
 import pytest
 
 from dotenv import load_dotenv
+
 load_dotenv(".env.example")
 
-from bots.settings import TOKEN_ADDRESS, STABLE_TOKEN_ADDRESS
+from bots.settings import TOKEN_ADDRESS
 from bots.data import Token, Price
+
 
 @pytest.mark.asyncio
 async def test_get_by_token_address():
     t = await Token.get_by_token_address(TOKEN_ADDRESS)
     assert t.token_address == TOKEN_ADDRESS
     assert t.listed == True
+
 
 @pytest.mark.asyncio
 async def test_get_prices():
